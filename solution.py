104719ac7417a90e3c500203fb16b51ce88fdc83
ai2066 <99619134+ai2066@users.noreply.github.com>
2022-02-26 19:48:32 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,46 +1,71 @@
 from socket import *
-import sys
 
 
-def webServer(port=13331):
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n Message Test by Ashiq Islam"
+    endmsg = "\r\n.\r\n"
 
-  serverSocket = socket(AF_INET, SOCK_STREAM)
- 
-  serverSocket.bind(("localhost", port)) 
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+    # Fill in start
+     clientSocket = socket(AF_INET, SOCK_STREAM)
+     clientSocket.connect(("127.0.0.1", port))
+    # Fill in end
 
-  serverSocket.listen(1)
+    recv = clientSocket.recv(1024).decode()
+    #print(recv)
+    #if recv[:3] != '220':
+        #print('220 reply not received from server.')
+
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    #print(recv1)
+    #if recv1[:3] != '250':
+        #print('250 reply not received from server.')
+
+    # Send MAIL FROM command and print server response.
+    # Fill in start
+    MAILFROM = "Mail From: <ashiq08401@gmail.com> \r\n"
+    clientSocket.send(MAILFROM.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    # Fill in end
+
+    # Send RCPT TO command and print server response.
+    # Fill in start
+    RCPTTO = "RCPT To: <ashiq08401@gmail.com> \r\n"
+    clientSocket.send(RCPTTO.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    # Fill in end
+
+    # Send DATA command and print server response.
+    # Fill in start
+    DATA = "DATA\r\n"
+    clientSocket.send(DATA.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    clientSocket.send(msg.encode())
+    clientSocket.send(endmsg.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    # Fill in end
+
+    # Message ends with a single period.
+    # Fill in start
+    clientSocket.send(endmsg.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    # Fill in end
+
+    # Send QUIT command and get server response.
+    # Fill in start
+    QUIT = "QUIT\r\n"
+    clientSocket.send(QUIT.encode())
+    recv7 = clientSocket.recv(1024).decode()
+    clientSocket.close()
+    # Fill in end
 
 
-  while True:
-    connectionSocket, addr = serverSocket.accept()
-    try:
-      try:
-        message = connectionSocket.recv(1024)
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = f.read()
-
-        messageok = 'HTTP/1.1 200 OK \r\n'
-        connectionSocket.send(messageok.encode())
-
-        #Send file 
-        for i in range(0, len(outputdata)):
-          connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-      except IOError:
-
-        # handle file not found (404)
-        messageNotOk = 'HTTP/1.1 404 Not Found\n\n'
-        connectionSocket.send(messageNotOk.encode())
-
-        connectionSocket.close()
-
-    except (ConnectionResetError, BrokenPipeError):
-      pass
-
-  serverSocket.close()
-  sys.exit()
-
-if __name__ == "__main__":
-  webServer(13331)
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
