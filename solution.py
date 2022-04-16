85f2b5dce2f244c6c1af4aa9c22af18c910b1186
ai2066 <99619134+ai2066@users.noreply.github.com>
2022-04-16 14:02:55 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -119,7 +119,7 @@ def ping(host, timeout=1):
 
     # Send ping requests to a server separated by approximately one second
     # Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
-	ttl = []
+    ttl = []
     for i in range(0, 4):  # Four pings will be sent (loop runs for i=0, 1, 2, 3)
         delay = doOnePing(dest, timeout)
         print(delay)
